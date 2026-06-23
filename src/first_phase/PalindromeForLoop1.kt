dd9d42a6acb2d46dec266f6c74984ba4772fa931
droidmac <dorid@thedroid.com>
2026-06-22 20:11:55 -0400

    Commits Palindrom with For loop

---
+++ src/first_phase/PalindromeForLoop1.kt
@@ -0,0 +1,33 @@
+package first_phase
+
+fun main(args: Array<String>) {
+
+//    val word = "Helloi Jorge!!"
+
+//    val word = "Jorge!!"
+
+//    val word = "azbcba"
+    val word = "abxycyxaba"
+
+    val result = isPalindrome(word, true)
+    println("Is  $word a palindrome!  $result")
+
+}
+
+fun isPalindrome(word: String, flag: Boolean): Boolean {
+
+    println(" With for loop ")
+
+    var end = word.length -1
+
+    for (i in 0 .. end) {
+
+        if (word[i] != word[end]  &&  i < end) {
+            return false
+        }
+        end--
+    }
+
+
+    return true
+}
