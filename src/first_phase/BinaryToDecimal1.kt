dd87b6871d50b5dcc73fdc385b8c1783751f33c7
droidmac <dorid@thedroid.com>
2026-06-28 14:15:59 -0400

    Adds fun toBinaryWithMutableList

--- src/first_phase/BinaryToDecimal1.kt
+++ src/first_phase/BinaryToDecimal1.kt
@@ -5,13 +5,34 @@ fun main(args: Array<String>) {
 
     val number = 824
 
-    val result = toBinaryString(number)
+//    val result = toBinaryWithStringBuilder(number)
+
+    val result = toBinaryWithMutableList(number)
+
 
     println("$number is $result")
 
 }
 
-fun toBinaryString(number: Int): String {
+fun toBinaryWithMutableList(number: Int): String {
+
+    val binaryString = mutableListOf<String>()
+    var newNumber = number
+
+    while (newNumber > 0) {
+
+        if ((newNumber % 2) == 0) {
+            binaryString.add("0")
+        } else {
+            binaryString.add("1")
+        }
+        newNumber /= 2
+    }
+
+    return binaryString.joinToString("").reversed()
+}
+
+fun toBinaryWithStringBuilder(number: Int): String {
 
     val binaryString = StringBuilder()
     var newNumber = number
