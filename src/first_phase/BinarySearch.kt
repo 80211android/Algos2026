a480ba67324e5776d54ce61d3b6ed87a1f8a58e1
droidmac <dorid@thedroid.com>
2026-06-29 16:23:15 -0400

    Adds if to prevent endless loop

--- src/first_phase/BinarySearch.kt
+++ src/first_phase/BinarySearch.kt
@@ -2,7 +2,7 @@ package first_phase
 
 fun main(args: Array<String>) {
 
-    val value = 273
+    val value = 31
 //    val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
     val numberList = listOf<Int>(20,22,31,51,57,67,75,75,76,81,90,113,115,119,129,131,145,146,163,187,200,205,208,222,225,227,240,240,247,252,257,260,263,266,267,273,292,311,352,362,370,416,419,424,437,441,458,463,465,467,470,486,488,506,518,530,531,557,566,590,601,614,617,631,639,641,644,648,651,664,691,692,695,698,711,715,732,734,759,778,793,797,802,807,811,816,838,842,858,865,881,889,894,925,947,949,970,982,984,1000)
 
@@ -25,6 +25,11 @@ fun searchNumer(numList: List<Int>, value: Int, start: Int, end: Int, counter: I
     val middle = ((end - start) / 2) + start
 
     println(" Counter: $newCounter .    ${numList[middle]}")
+
+    if (newCounter > numList.size) {
+        println("value   $value NOOT found at all ")
+        return
+    }
 
 
     if (value < numList[middle]) {
