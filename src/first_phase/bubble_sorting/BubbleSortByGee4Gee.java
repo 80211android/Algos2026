8a64f6f3e701105471ca7f4792ebe26118eb915c
droidmac <dorid@thedroid.com>
2026-07-03 13:20:10 -0400

    Commits simple changes

--- src/first_phase/bubble_sorting/BubbleSortByGee4Gee.java
+++ src/first_phase/bubble_sorting/BubbleSortByGee4Gee.java
@@ -2,23 +2,27 @@ package first_phase.bubble_sorting;
 
 // Optimized java implementation of Bubble sort
 import java.io.*;
+import java.util.Arrays;
 
 public class BubbleSortByGee4Gee {
 
 
     // An optimized version of Bubble Sort
-    static void bubbleSort(int arr[], int n){
+    static void bubbleSort(int arr[], int length){
+        int counter = 0;
         int i, j, temp;
         boolean swapped;
-        for (i = 0; i < n - 1; i++) {
+        for (i = 0; i < length - 1; i++) {
             swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
+//            for (j = 0; j < length - i - 1; j++) {
+            for (j = 0; j < length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
 
                     // Swap arr[j] and arr[j+1]
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    counter++;
                     swapped = true;
                 }
             }
@@ -28,6 +32,8 @@ public class BubbleSortByGee4Gee {
             if (swapped == false)
                 break;
         }
+
+        System.out.println("Counter is: " + counter);
     }
 
     // Function to print an array
@@ -40,7 +46,8 @@ public class BubbleSortByGee4Gee {
 
     // Driver program
     public static void main(String args[]){
-        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+//        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        int arr[] = { 83, 17, 42, 95, 6, 61, 28, 74, 39, 52, 11, 88, 33, 67, 24, 79, 45, 13, 56, 91, 38, 72, 5, 49, 84, 21, 63, 97, 30, 58, 14, 76, 47, 22, 69, 35, 81, 9, 54, 93, 26, 48, 71, 16, 60, 37, 85, 43, 8, 66};
         int n = arr.length;
 
         printArray(arr, n);
--- src/first_phase/bubble_sorting/MyBubbleSortOne.kt
+++ src/first_phase/bubble_sorting/MyBubbleSortOne.kt
@@ -2,7 +2,7 @@ package first_phase.bubble_sorting
 
 fun main(args: Array<String>) {
 
-//    val list = listOf<Int>(7, 5, 2, 9, 0)
+//    val list = listOf<Int>(64, 34, 25, 12, 22, 11, 90 )
     val list = listOf(83, 17, 42, 95, 6, 61, 28, 74, 39, 52, 11, 88, 33, 67, 24, 79, 45, 13, 56, 91, 38, 72, 5, 49, 84, 21, 63, 97, 30, 58, 14, 76, 47, 22, 69, 35, 81, 9, 54, 93, 26, 48, 71, 16, 60, 37, 85, 43, 8, 66)
 
     println(list)
@@ -14,6 +14,7 @@ fun bubbleSort(theNumberList: List<Int>) {
 
     val numberList = theNumberList.toMutableList()
     var counter = 0
+    var loopCounter = 0
 
     while (counter < numberList.size - 1) {
 
@@ -22,6 +23,7 @@ fun bubbleSort(theNumberList: List<Int>) {
             numberList[counter] = numberList[counter + 1]
             numberList[counter + 1] = holder
             counter = 0
+            loopCounter++
 
         } else {
             counter++
@@ -34,7 +36,7 @@ fun bubbleSort(theNumberList: List<Int>) {
         print(" $number, ")
     }
 
-    println(" **** . ")
+    println(" \n **** .  Loop cunter $loopCounter ***  \n")
 
     for (number in numberList) {
         print(" $number, ")
