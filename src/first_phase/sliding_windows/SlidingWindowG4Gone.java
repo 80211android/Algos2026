56ba9bc8d83c1fa605b84b3881d461576c028fec
droidmac <dorid@thedroid.com>
2026-07-07 20:23:44 -0400

    Renames variables

--- src/first_phase/sliding_windows/SlidingWindowG4Gone.java
+++ src/first_phase/sliding_windows/SlidingWindowG4Gone.java
@@ -2,25 +2,25 @@ package first_phase.sliding_windows;
 
 public class SlidingWindowG4Gone {
 
-    static int maxSum(int arr[], int n, int k){
-        // n must be greater
-        if (n <= k) {
+    static int maxSum(int array[], int arraySize, int sizeOfCounter){
+        // arraySize must be greater
+        if (arraySize <= sizeOfCounter) {
             System.out.println("Invalid");
             return -1;
         }
 
-        // Compute sum of first window of size k
+        // Compute sum of first window of size sizeOfCounter
         int max_sum = 0;
-        for (int i = 0; i < k; i++)
-            max_sum += arr[i];
+        for (int i = 0; i < sizeOfCounter; i++)
+            max_sum += array[i];
 
         // Compute sums of remaining windows by
         // removing first element of previous
         // window and adding last element of
         // current window.
         int window_sum = max_sum;
-        for (int i = k; i < n; i++) {
-            window_sum += arr[i] - arr[i - k];
+        for (int i = sizeOfCounter; i < arraySize; i++) {
+            window_sum += array[i] - array[i - sizeOfCounter];
             max_sum = Math.max(max_sum, window_sum);
         }
 
