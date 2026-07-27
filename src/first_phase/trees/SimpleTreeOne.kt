cf57184f9986570ddfe7d12db483444589b4a248
droidmac <dorid@thedroid.com>
2026-07-27 13:28:18 -0400

    Adds Tree One Data Structure

---
+++ src/first_phase/trees/SimpleTreeOne.kt
@@ -0,0 +1,65 @@
+package first_phase.trees
+
+import first_phase.string_contains_substring.findSubstring
+
+var startNode: NodeTree? = null
+
+fun main(args: Array<String>) {
+
+    val value = listOf<Int>(1, 2, 3, 4, 5)
+
+    value.forEach { value ->
+        addToTree(value)
+    }
+
+    traverseAndPrintOne(startNode)
+
+}
+
+fun traverseAndPrintOne(node: NodeTree?) {
+    if (node != null) {
+        println(" Node value: ${node.value}  \n")
+
+        if (node.leftNodeTree != null) {
+            traverseAndPrintOne(node.leftNodeTree)
+        }
+
+        if (node.rightNodeTree != null) {
+            traverseAndPrintOne(node.rightNodeTree)
+        }
+
+    }
+}
+
+fun addToTree(value: Int): Unit {
+
+    if (startNode == null) {
+        println(" Adding to node  $value  to initial node. ")
+        startNode = NodeTree(value)
+    } else startNode?.let {
+        addValueToNode(value, it)
+    }
+
+}
+
+fun addValueToNode(value: Int, node: NodeTree) {
+
+    if (node.leftNodeTree == null) {
+        println(" Adding to node  $value  to  left")
+
+        node.leftNodeTree = NodeTree(value)
+    } else if (node.rightNodeTree == null) {
+        println(" Adding to node  $value  to right")
+
+        node.rightNodeTree = NodeTree(value)
+    } else {
+        addValueToNode(value, node.leftNodeTree!!)
+    }
+}
+
+
+data class NodeTree(
+    val value: Int,
+    var leftNodeTree: NodeTree? = null,
+    var rightNodeTree: NodeTree? = null,
+)
